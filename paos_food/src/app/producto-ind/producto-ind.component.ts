cdc304856ae80a174a4b20af555f4edc2910cc7b
Jorge Vulgarin <adrivulgarin@hotmail.com>
2023-06-26 07:44:33 -0500

    producto-ind terminado

--- paos_food/src/app/producto-ind/producto-ind.component.ts
+++ paos_food/src/app/producto-ind/producto-ind.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Producto} from '../producto';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Producto } from '../producto';
 import { ProductService } from '../services/product/product.service';
+import { AddProductService } from '../services/cart/add-product.service';
 
 @Component({
   selector: 'app-producto-ind',
@@ -11,31 +12,39 @@ import { ProductService } from '../services/product/product.service';
 export class ProductoIndComponent {
 
   product!: Producto;
+  total!: number;
+  item_count!: number;
 
-  constructor(private route: ActivatedRoute, public productService: ProductService ) {
+  increment_product_count() {
+    this.item_count++;
+    this.total = this.product.price * this.item_count;
+  }
+  decrement_product_count() {
+    if (this.item_count === 0) return;
+    this.item_count--;
+    this.total = this.product.price * this.item_count;
+  }
+  add_to_cart(){
+    this.cartService.addToCart(this.product.id, this.item_count).subscribe((res:any)=>{
+      if (res["success"] as boolean){
+        this.router.navigate(["/carrito"])
+      }
+    })
+  }
+
+  constructor(private route: ActivatedRoute, private router: Router, public productService: ProductService, public cartService:AddProductService) {
+    this.item_count = 0;
+    this.total = 0.00;
 
     this.route.params.subscribe(params => {
 
-      let id:string = params['id'];
+      let id: string = params['id'];
 
-      this.productService.getProductsById(id).subscribe((res: any)=>{
-        let product:Producto = res["product"] as Producto
+      this.productService.getProductsById(id).subscribe((res: any) => {
+        let product: Producto = res["product"] as Producto
         this.product = product;
 
       })
-
-      // let productos= JSON.parse(localStorage.getItem('productos')!);
-      //
-      // const array = Object.values(productos);
-      //
-      // if (productos){
-      //   let arrProd=array as Array<Producto>;
-      //
-      //   let prodFilt=arrProd.filter(producto => producto.name == id);
-      //
-      //   this.product=prodFilt[0];
-      //
-      // }
 
     });
   }
---
+++ paos_food/src/app/services/cart/add-product.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddProductService {
+  apiURL = "http://127.0.0.1:8000/api/cart"
+
+  constructor(private http:HttpClient) { }
+
+  getCart():Observable<any>{
+    return this.http.get<any>(`${this.apiURL}/`)
+  }
+  addToCart(product_id:number, quantity:number):Observable<any>{
+    return this.http.post<any>(`${this.apiURL}/`,{"product":product_id,"quantity":quantity})
+  }
+}
