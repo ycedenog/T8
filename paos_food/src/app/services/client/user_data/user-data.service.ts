63cbc49d3556d3939973a8e2183a014cc23bfdd8
Jorge10542001 <adrivuglarin@hotmail.com>
2023-08-14 02:06:05 -0500

    services to query user data

---
+++ paos_food/src/app/services/client/user_data/user-data.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserDataService {
+  apiURL = "https://jorge15042001.pythonanywhere.com/accounts/api_login";
+
+  constructor(private http: HttpClient, private cookies: CookieService) { }
+  userData(httpOptions: any): Observable<any> {
+    return this.http.get(this.apiURL, httpOptions);
+  }
+}
