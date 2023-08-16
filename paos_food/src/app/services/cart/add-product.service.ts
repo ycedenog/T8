3b3408e1541cbabf29bb5fa8f6e2d82ee0992cbd
Jorge10542001 <adrivuglarin@hotmail.com>
2023-08-15 23:50:58 -0500

    fix: apiURL for add-product

--- paos_food/src/app/services/cart/add-product.service.ts
+++ paos_food/src/app/services/cart/add-product.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AddProductService {
-  apiURL = "http://127.0.0.1:8000/api/cart"
+  apiURL = "/api/cart"
 
   constructor(private http:HttpClient) { }
 
