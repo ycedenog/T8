a4ebde4c94a21fe16ba83af98a5d92464538a8a2
Jorge10542001 <adrivuglarin@hotmail.com>
2023-08-16 23:46:54 -0500

    added callbacks for adding the reducing cart items

--- paos_food/src/app/shop-cart/item-cart/item-cart.component.ts
+++ paos_food/src/app/shop-cart/item-cart/item-cart.component.ts
@@ -1,5 +1,7 @@
+import { HttpResponse } from '@angular/common/http';
 import { Component, Input } from '@angular/core';
 import { CartItem } from 'src/app/cart_item';
+import { AddProductService } from 'src/app/services/cart/add-product.service';
 
 @Component({
   selector: 'tr[app-item-cart]',
@@ -8,5 +10,28 @@ import { CartItem } from 'src/app/cart_item';
 })
 export class ItemCartComponent {
   @Input() cartItem!: CartItem;
+
+  constructor(public cartService:AddProductService){
+  }
+
+  delete(){
+  }
+  augmentQuantity(){
+    this.cartService.addToCart(this.cartItem.product.id, 1).subscribe((res:HttpResponse<any>)=>{
+      if(res.status !=200){return;}
+      if(!res.body["success"]){return;}
+      console.log(res.body)
+      this.cartItem.quantity += 1;
+    })
+
+  }
+  reduceQuantity(){
+    this.cartService.addToCart(this.cartItem.product.id, -1).subscribe((res:HttpResponse<any>)=>{
+      if(res.status !=200){return;}
+      if(!res.body["success"]){return;}
+      console.log(res.body)
+      this.cartItem.quantity -= 1;
+    })
+  }
 
 }
