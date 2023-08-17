f86e23c7f69db559010c6020e8ad80ee864264df
Jorge10542001 <adrivuglarin@hotmail.com>
2023-08-17 00:11:41 -0500

    add observer and subscriber to react to cart-item deletion

--- paos_food/src/app/shop-cart/shop-cart.component.ts
+++ paos_food/src/app/shop-cart/shop-cart.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AddProductService } from '../services/cart/add-product.service';
 import { CartItem } from '../cart_item';
 import { HttpResponse } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-shop-cart',
@@ -12,6 +13,9 @@ export class ShopCartComponent {
   cart!: CartItem[];
   total_payment: number=0;
 
+  public _removeCartItemSub: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+  public removeCartItemObs: Observable<number> = this._removeCartItemSub.asObservable();
+
   constructor(public cartService: AddProductService) {
     this.cartService.getCart().subscribe((res:  HttpResponse<any>) => {
       if (res.status != 200){
@@ -21,6 +25,10 @@ export class ShopCartComponent {
       for (let cartItem of this.cart){
         this.total_payment += cartItem.quantity * cartItem.product.price;
       }
+
+      this.removeCartItemObs.subscribe((cart_item_id:number)=>{
+        this.cart = this.cart.filter((cart_item:CartItem)=>cart_item_id!==cart_item.id)
+      })
     })
   }
 
