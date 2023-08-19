8ae7580cb529e42da1047cd0a51b5451ed15b733
Jorge10542001 <adrivuglarin@hotmail.com>
2023-08-18 19:32:08 -0500

    fix: wrong apiURL

--- paos_food/src/app/services/client/signup/singup.service.ts
+++ paos_food/src/app/services/client/signup/singup.service.ts
@@ -7,7 +7,7 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class SingupService {
 
-  apiURL = "https://jorge15042001.pythonanywhere.com/accounts/api_signup";
+  apiURL = "/accounts/api_signup";
 
 
   constructor(private http: HttpClient, private cookies : CookieService) { }
